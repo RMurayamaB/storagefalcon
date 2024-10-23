49a634dc7c3c98c21aaba7b9b72b72c1781a14b8
RMurayamaB <rafaelmurayama@hotmail.com>
2024-10-23 00:11:26 -0300

    :ok_hand: change home.js

--- frontend/scripts/home.js
+++ frontend/scripts/home.js
@@ -2,14 +2,10 @@ import Modal from './components/Modal.js';
 import FolderManager from './components/FolderManager.js';
 import CreateFolder from './components/CreateFolder.js';
 
-
 document.addEventListener('DOMContentLoaded', () => {
   const modalElement = document.querySelector('.modal');
   const closeButton = document.querySelector('.close-modal');
   const createFolderButton = document.querySelector('.btn-create');
-  const folderNameInput = document.querySelector('#folder-name');
-  const errorMessage = document.querySelector('.error-message');
-  const tableFolders = document.querySelector('.table-body');
 
   const modalComponent = new Modal(modalElement, closeButton);
 
